3995a9cab0718ba3cb30fb08c99a3f923ea6b13b
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-19 15:15:03 +0530

    story 13- variable a & b declaration inside function and inside main program dif

--- frontend.sh
+++ frontend.sh
@@ -2,26 +2,30 @@
  log_file=/tmp/roboshop.log
  rm -f ${log_file}
 
+print_head(){
 
- echo -e "\e[35minstalling nginx\e[0m"
+  echo -e "\e[35m$1\e[0m"
+}
+
+ print_head "installing nginx"
  yum install nginx -y &>>${log_file}
 
- echo -e "\e[35mRemoving old content\e[0m"
+ print_head "Removing old content"
  rm -rf /usr/share/nginx/html/* &>>${log_file}
 
- echo -e "\e[35mDownloading frontend content from browser\e[0m"
+ print_head "Downloading frontend content from browser"
  curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
- echo -e "\e[35mExtracting downloaded Frontend\e[0m"
+ print_head "Extracting downloaded Frontend"
  cd /usr/share/nginx/html
  unzip /tmp/frontend.zip &>>${log_file}
 
- echo -e "\e[35mCopying nginx config for roboshop\e[0m"
+ print_head "Copying nginx config for roboshop"
  cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
- echo -e "\e[35mEnabling nginx\e[0m"
+ print_head "Enabling nginx"
  systemctl enable nginx &>>${log_file}
- echo -e "\e[35mRestarting nginx\e[0m"
+ print_head "Restarting nginx"
  systemctl restart nginx &>>${log_file}
 
 
