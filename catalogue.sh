193756810a924b4908765697bdd3aea577496042
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-20 12:41:02 +0530

    story 16- catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,22 +1,42 @@
 source common.sh
 
+print_head "configure nodejs repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+print_head "installing nodejs"
+yum install nodejs -y &>>${log_file}
+useradd roboshop &>>${log_file}
+
+print_head "making application Directory"
+mkdir /app &>>${log_file}
+
+print_head "Removing old content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading app content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp configs/catalogue.service /etc/systemd/system/catalogue.service
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+print_head "Exctarcting app content"
+unzip /tmp/catalogue.zip &>>${log_file}
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.kalluriravidevops71.online</app/schema/catalogue.js
+print_head "installing nodejs dependencies"
+npm install &>>${log_file}
+
+print_head "copying systemd service file of catalogue"
+cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reloading the systemd service"
+systemctl daemon-reload &>>${log_file}
+print_head "enable catalogue"
+systemctl enable catalogue &>>${log_file}
+print_head "start catalogue"
+systemctl restart catalogue &>>${log_file}
+
+print_head "copying monogdb repository"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "installing mongodb client"
+yum install mongodb-org-shell -y &>>${log_file}
+print_head "Load schema"
+mongo --host mongodb.kalluriravidevops71.online</app/schema/catalogue.js &>>${log_file}
--- mongodb.sh
+++ mongodb.sh
@@ -6,10 +6,11 @@ cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 print_head " installing mongodb"
 yum install mongodb-org -y &>>${log_file}
 
-print_head " enable mongodb"
+print_head "enable mongodb"
 systemctl enable mongod &>>${log_file}
 
-print_head " start mongodb" &>>${log_file}
+print_head "start mongodb" &>>${log_file}
 systemctl start mongod
+
 
 # Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
