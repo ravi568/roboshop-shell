16ef8dd7618907c31465f9e0fcdd380bfba91d6a
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-21 16:38:39 +0530

    story 30- adding fucntions to make code dry schema,systemd, app_prereq,java

--- common.sh
+++ common.sh
@@ -90,6 +90,7 @@ app_prereq_setup(){
     status_check $?
 }
 nodejs() {
+
   print_head "Configure NodeJS Repo"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
   status_check $?
